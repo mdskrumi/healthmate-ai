69bedabd51bdf7ea7ffd5275a47c1fba55270c48
Md. Sakibul Alam <mdskrumi@gmail.com>
2023-06-07 15:07:24 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Dictaphone from "./components/dictaphone";
+
+function App() {
+  return (
+    <div className="App">
+      <Dictaphone />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/dictaphone.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+const Dictaphone = () => {
+  const [speaker, setSpeaker] = useState<"Doctor" | "Patient" | "None">("None");
+  const [message, setMessage] = useState<
+    {
+      speaker: "Doctor" | "Patient";
+      message: string;
+    }[]
+  >([]);
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  const handleSpeakerChange = (person: "Doctor" | "Patient") => {
+    if (speaker !== "None") {
+      setMessage((prev) => [
+        ...prev,
+        { speaker: speaker, message: transcript },
+      ]);
+    }
+    SpeechRecognition.stopListening();
+    if (person === speaker) {
+      setSpeaker("None");
+      resetTranscript();
+    } else {
+      setSpeaker(person);
+      resetTranscript();
+      SpeechRecognition.startListening({ continuous: true });
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex justify-center items-center gap-10 p-10">
+        <button
+          onClick={() => handleSpeakerChange("Doctor")}
+          className={`block max-w-sm p-6 border border-gray-200 rounded-3xl shadow ${
+            speaker === "Doctor" ? "bg-green-300" : "bg-white hover:bg-gray-100"
+          }`}
+        >
+          <h2 className="font-bold p-10">DOCTOR</h2>
+        </button>
+        <button
+          onClick={() => handleSpeakerChange("Patient")}
+          className={`block max-w-sm p-6 border border-gray-200 rounded-3xl shadow ${
+            speaker === "Patient"
+              ? "bg-green-300"
+              : "bg-white hover:bg-gray-100"
+          }`}
+        >
+          <h2 className="font-bold p-10">PATIENT</h2>
+        </button>
+      </div>
+
+      <div className="m-auto max-w-2xl w-[90vw] border-2 p-5">
+        {message.map((m, i) => (
+          <div
+            key={i}
+            className={`${m.speaker === "Doctor" ? "text-left" : "text-right"}`}
+          >{`${m.speaker}: ${m.message}.`}</div>
+        ))}
+      </div>
+      <div className="m-auto max-w-2xl w-[90vw] p-5">
+        <button className="ml-auto block max-w-sm p-4 border border-gray-200 rounded-xl shadow hover:bg-gray-100">
+          SUBMIT
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Dictaphone;
