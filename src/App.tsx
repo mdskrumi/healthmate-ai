ae1e740c325e0a2a93adbcceb4d5b04807891ba5
Md. Belal Hossain <belal.cseai@gmail.com>
2023-06-12 21:15:21 +0600

    adds routes, outlet and basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import Dictaphone from "./components/dictaphone";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import DashboardOutlet from "./components/DashboardOutlet";
+import NewSession from "./components/NewSession";
+import SessionHistory from "./components/SessionHistory";
 
 function App() {
   return (
     <div className="App">
-      <Dictaphone />
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<DashboardOutlet />}>
+            <Route path={"/"} element={<Dictaphone />} />
+            <Route path={"session"} element={<NewSession />} />
+            <Route path={"history"} element={<SessionHistory />} />
+          </Route>
+          <Route path="*" element={<Navigate to={"/"} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/DashboardOutlet.tsx
@@ -0,0 +1,183 @@
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { SvgIconComponent } from "@mui/icons-material";
+
+import { ArrowDropDown, VoiceChat, History } from "@mui/icons-material";
+
+import logo from "../assets/logo.png";
+
+const drawerWidth = 240;
+
+interface IMenu {
+  id: string;
+  title: string;
+  url: string;
+  icon: SvgIconComponent;
+}
+
+const menus: IMenu[] = [
+  {
+    id: "session",
+    title: "New Session",
+    url: "/session",
+    icon: VoiceChat,
+  },
+  {
+    id: "history",
+    title: "Session History",
+    url: "/history",
+    icon: History,
+  },
+];
+
+const DashboardOutlet: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [selectedMenu, setSelectedMenu] = useState<null | string>(null);
+
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOnMenuSelected = (menu: IMenu) => {
+    setSelectedMenu(menu.id);
+    navigate(menu.url);
+  };
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setSelectedMenu(null);
+    }
+  }, [location.pathname]);
+
+  return (
+    <Box sx={{ display: "flex", bgcolor: "background.default" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: `calc(100% - ${drawerWidth}px)`,
+          ml: `${drawerWidth}px`,
+          // backgroundColor: "trans",
+        }}
+        color="default"
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, marginLeft: "16px" }}
+          >
+            <div>Healthmate</div>
+            <div className="text-sm">AI solution for Doctor and Patioent</div>
+          </Typography>
+          <div>
+            <IconButton size="large" edge="end" color="inherit">
+              <AccountCircleIcon />
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+              color="inherit"
+            >
+              <ArrowDropDown />
+            </IconButton>
+          </div>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar>
+          <div onClick={() => navigate("/")}>
+            <img src={logo} alt="Logo" />
+          </div>
+        </Toolbar>
+        <Divider />
+        <List>
+          {menus.map((item, _) => (
+            <ListItem
+              key={item.title}
+              disablePadding
+              className={
+                selectedMenu && selectedMenu === item.id ? "bg-gray-100" : ""
+              }
+            >
+              <ListItemButton onClick={() => handleOnMenuSelected(item)}>
+                <ListItemIcon>{<item.icon />}</ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+      >
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default DashboardOutlet;
